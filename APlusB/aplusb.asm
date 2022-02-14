a9297b74cb18fe984807da3fbaca703ce7c43052
Albert Nepomnyashchy <albertnepomnyashiy@gmail.com>
2022-02-14 20:01:10 +0700

    a+b seems to be working

--- APlusB/aplusb.asm
+++ APlusB/aplusb.asm
@@ -1,8 +1,11 @@
 global _start
 
 section .bss
-input   resb    0xff                ; input length
+input1  resb    0xff
+input2  resb    0xff
 output  resb    0xff
+paramA  resd    1
+paramB  resd    1
 result  resd    1
 
 section .text
@@ -11,68 +14,73 @@ section .text
 ; rdx = pointer to string to convert
 ; Output:
 ; rax = converted integer
-_atoi:  mov     rdx, input          
-        xor     rax, rax            ; zero a "result so far"
+_atoi:  xor     rax, rax            ; zero a "result so far"
 .nxtch: movzx   rcx, byte [rdx]     ; get a character
         inc     rdx                 ; ready for next one
         cmp     rcx, '0'            ; valid?
-        jb      .done               ; 
+        jb      .done               ;
         cmp     rcx, '9'            ; valid?
         ja      .done               ;
         sub     rcx, '0'            ; "convert" character to number
         imul    rax, 10             ; multiply "result so far" by ten
         add     rax, rcx            ; add to result
         jmp     .nxtch              ; until done
-.done:  mov     [result], rax
-        ret
+.done:  ret
 
 ; Reads up to 0xff symbols from stdin
 ; Input:
 ; rsi = pointer to buffer to write to
 _input: mov     rax, 0
         mov     rdi, 0
-        mov     rsi, input
         mov     rdx, 0xff           ; some length
         syscall
         ret
 
 ; Writes up to 0xff symbols to stdout
 ; Input:
-; rso = pointer to buffer to read from
+; rsi = pointer to buffer to read from
 _print: mov     rax, 1
         mov     rdi, 1
-        mov     rsi, output
-        mov     rdx, 0xff           ; some length
+        ;mov     rdx, 0xff           ; some length
         syscall
         ret
 
 ; Converts integer to string (e.g. for sys_write input) 
 ; Input:
-; eax = integer value to convert
-; esi = pointer to buffer to store the string in (must have room for at least 10 bytes)
+; rax = integer value to convert
+; rsi = pointer to buffer to store the string in
 ; Output:
-; eax = pointer to the first character of the generated string
-; ecx = length of the generated string
-_itoa:  mov     rax, [result]
-        mov     rsi, output
-        add     rsi, 255
+; rax = pointer to the first character of the generated string
+; rcx = length of the generated string
+_itoa:  add     rsi, 240
         mov     byte [rsi], 0       ; string terminator
         mov     rbx, 10
+        xor     rcx, rcx
 .nxtdg: xor     rdx, rdx
-        div     rbx                 ; eax /= 10
+        div     rbx                 ; rax /= 10
         add     dl, '0'             ; Convert the remainder to ASCII 
         dec     rsi                 ; store characters in reverse order
         mov     [rsi], dl
-        test    rax, rax            
-        jnz     .nxtdg              ; until eax==0
-        
-        mov rax, rsi
+        inc     rcx
+        test    rax, rax
+        jnz     .nxtdg              ; until rax==0
+        mov     rax, rsi
         ret
 
-_start: call    _input
+_start: mov     rsi, input1
+        call    _input
+        mov     rdx, input1
         call    _atoi
-        inc     dword [result]
+        mov     [paramA], rax
+        mov     rsi, input2
+        call    _input
+        mov     rdx, input2
+        call    _atoi
+        add     rax, [paramA]
+        mov     rsi, output
         call    _itoa
+        mov     rdx, rcx
+        mov     rsi, rax
         call    _print
         mov rax, 60
         mov rdi, 0
