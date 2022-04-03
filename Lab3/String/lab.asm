4a6d469d8cc02cb4659a9eb0378a3abbd0970660
Albert Nepomnyashchy <albertnepomnyashiy@gmail.com>
2022-04-04 01:07:44 +0700

    Lab3 fix and cleanup

--- Lab3/String/lab.asm
+++ Lab3/String/lab.asm
@@ -1,14 +1,8 @@
-extern printf
-
 global  main
 
 section .bss
 string  resb    0xff
 n       resq    1
-array   resq    0
-
-section .data
-fmt:    db      "%lld", 10, 0
 
 section .text
 
@@ -27,43 +21,22 @@ _input: mov     rax, 0
 ; rdx = number of symbols to print
 _output:mov     rax, 1
         mov     rdi, 1
-        ;mov     rdx, 0xff           ; some length
         syscall
         ret
 
-; Calls printf() function to write an integer
-; Input:
-; rax = integer to write
-_prntf: mov     rdi, fmt
-        mov     rsi, rax
-        call    printf
-        ret
-
-
 main:   mov     rsi, string
         call    _input
-        ;dec     rax
         mov     [n], rax
-        ;call    _prntf
-
         xor     rdi, rdi
-
         mov     rdi, string
         mov     rcx, [n]
         mov     al, ' '
         cld
         repnz   scasb
-        ;dec     rsi
-        cmp     rcx, 0
-        jne     .found
-        mov     rdi, string
-        mov     rdx, [n]
-        jmp     .result
-
-.found: mov     rdx, rcx
+        mov     rdx, rcx
         mov     rsi, rdi
 
-.result:call    _output
+        call    _output
         
         mov rax, 60
         mov rdi, 0
